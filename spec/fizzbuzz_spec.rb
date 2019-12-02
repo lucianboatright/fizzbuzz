1971c9af7539a9739c78b9c2fc852d261cded9e6
Lucian Boatright <lucian.boatright@gmail.com>
2019-12-02 13:07:41 +0000

    Added test for =15 and code for %5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,13 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when input is 6' do
     expect(fizzbuzz(6)).to eq 'fizz'
   end
+  it 'returns "buzz" when input is 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+    it 'returns "buzz" when input is 5' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+      it 'returns "fizzbuzz" when input is 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
 end
