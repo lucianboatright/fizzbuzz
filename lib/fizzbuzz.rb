abd2fb72e2d009e7e9a445360143488e09ef03c9
Tom <ratcliffetj89@gmail.com>
2019-12-02 13:19:41 +0000

    added multiple of 3 and 5 code and non multiple of 3 or 5 code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,14 @@
 def fizzbuzz(number)
-      if number % 3 == 0
-        "fizz"
-      elsif number % 5 == 0
-        "buzz"
-      end
+  if number % 3 == 0 && number % 5 == 0
+    "fizzbuzz"
+  elsif number % 3 == 0
+    "fizz"
+  elsif number % 5 == 0
+    "buzz"
+  else
+    number
+  end
 end
-
-
-
 
 
 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,10 +10,16 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when input is 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
-    it 'returns "buzz" when input is 5' do
+  it 'returns "buzz" when input is 5' do
     expect(fizzbuzz(10)).to eq 'buzz'
   end
-      it 'returns "fizzbuzz" when input is 15' do
+  it 'returns "fizzbuzz" when input is 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns number when input is not a multiple of 3 or 5' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+  it 'returns number when input is not a multiple of 3 or 5, second example' do
+    expect(fizzbuzz(17)).to eq 17
   end
 end
