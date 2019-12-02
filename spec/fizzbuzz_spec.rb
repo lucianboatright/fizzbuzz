70d6975f7d50f29ae01b925eb172608be7f9286c
Tom <ratcliffetj89@gmail.com>
2019-12-02 13:00:11 +0000

    added multiple of 3 test to sspec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,8 +1,10 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do 
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when input is 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "fizz" when input is 6' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
 end
-
